039fd14faa709c22d9c394ea691e18fde3e98e8e
tharique2132000 <59535707+tharique2132000@users.noreply.github.com>
2023-03-30 23:30:40 +0530

    Changed the loading text to typing text

--- script.js
+++ script.js
@@ -1,61 +1,64 @@
-        var tablinks = document.getElementsByClassName("tab-links");
-        var tabcontents = document.getElementsByClassName("tab-contents");
-        function opentab(tabname){
-            for(tablink of tablinks){
-                tablink.classList.remove("active-link");
-            }
-             for (tabContent of tabcontents) {
-                tabContent.classList.remove("active-tab");
-            }
-            event.currentTarget.classList.add("active-link");
-            document.getElementById(tabname).classList.add("active-tab")
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabContent of tabcontents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
 
-        }
+// ---------------------------Loading text--------------------------
 
+//         const initialText = "KGTYUFVJE";
+//         const welcomeText = "DEVELOPER";
+//         // const welcomeText = document.getElementById("welcome-text");
+//         const textElement = document.getElementById("welcome-text");
+//         let loadedText = initialText;
+//         let currentIndex = 0;
 
+//         function loadRandomLetters() {
+//             let randomLetters = "";
+//             for (let i = 0; i < welcomeText.length; i++) {
+//                 randomLetters += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+//             }
+//             loadedText = randomLetters;
+//             textElement.innerHTML = loadedText;
+//             currentIndex = 0;
+//             setTimeout(loadWelcomeText, 3);
+//         }
 
-        // ---------------------------Loading text--------------------------
+//         function loadWelcomeText() {
+//             if (loadedText[currentIndex] !== welcomeText[currentIndex]) {
+//                 let newLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+//                 loadedText = loadedText.substr(0, currentIndex) + newLetter + loadedText.substr(currentIndex + 1);
+//                 textElement.innerHTML = loadedText;
+//                 setTimeout(loadWelcomeText, 3);
+//             } else {
+//                 currentIndex++;
+//                 if (currentIndex === welcomeText.length) {
+//                     return;
+//                 }
+//                 setTimeout(loadWelcomeText, 3);
+//             }
+//         }
 
-        const initialText = "KGTYUFVJE";
-        const welcomeText = "DEVELOPER";
-        // const welcomeText = document.getElementById("welcome-text");
-        const textElement = document.getElementById("welcome-text");
-        let loadedText = initialText;
-        let currentIndex = 0;
+//         function startLoading() {
+//             loadRandomLetters();
+//         }
 
-        function loadRandomLetters() {
-            let randomLetters = "";
-            for (let i = 0; i < welcomeText.length; i++) {
-                randomLetters += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-            }
-            loadedText = randomLetters;
-            textElement.innerHTML = loadedText;
-            currentIndex = 0;
-            setTimeout(loadWelcomeText, 3);
-        }
+// setInterval(() => {
+//             startLoading();
+// 			loadText(target, finalText);
+// }, 4000);
 
-        function loadWelcomeText() {
-            if (loadedText[currentIndex] !== welcomeText[currentIndex]) {
-                let newLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-                loadedText = loadedText.substr(0, currentIndex) + newLetter + loadedText.substr(currentIndex + 1);
-                textElement.innerHTML = loadedText;
-                setTimeout(loadWelcomeText, 3);
-            } else {
-                currentIndex++;
-                if (currentIndex === welcomeText.length) {
-                    return;
-                }
-                setTimeout(loadWelcomeText, 3);
-            }
-        }
-
-        function startLoading() {
-            loadRandomLetters();
-        }
-
-setInterval(() => {
-            startLoading();
-			loadText(target, finalText);
-		}, 4000); 
-
-        
+// ---------------------------------------------------------------new typing animaation-----------------------------
+var typed = new Typed(" .welcome-text", {
+  strings: ["Full Stack Developer.", "Content Creator.", "Designer."],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
